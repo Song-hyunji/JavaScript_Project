2e72f3a89ba9d81141aefe03e599fca87ee08ae3
Song-hyunji <h3707@naver.com>
2021-09-18 22:03:06 +0900

    finished ~4.7

---
+++ app.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const link = document.querySelector("a");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY ="username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings();
+}
+
+function paintGreetings(username){
+    const localUsername = localStorage.getItem(USERNAME_KEY);
+    greeting.innerHTML = `Hello ${localUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+console.log(savedUsername);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    paintGreetings();
+}
